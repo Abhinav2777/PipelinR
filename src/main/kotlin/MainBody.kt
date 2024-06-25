ed2279e9d5b96ab9235990a1c70b4f07438185ba
Vikas <rapellyvikas1971@gmail.com>
2024-06-25 17:34:56 +0530

    Adding more modularity to the code

---
+++ src/main/kotlin/MainBody.kt
@@ -0,0 +1,20 @@
+import java.lang.Exception
+
+fun mainBody(command: MyCommand): String {
+    println("Executing the main logic.....")
+
+    logger.debug("This is an Debug Message!")
+    logger.info("This is an Info Message!")
+    logger.warn("This is an Warn Message!")
+    logger.error("This is an Error Message!")
+    logger.fatal("This is an Fatal Message!")
+    logger.trace("This is an Trace Message!")
+    try{
+        logger.trace("Entering the Try block")
+        println(100/0)
+    }
+    catch(e : Exception){
+        logger.error("Encountered the exception ${e.message}")
+    }
+    return "${command.data} is authorized to this code, finished main block"
+}
