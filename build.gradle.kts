b5fbd5835dcfc4ccad1fa21d83252b83558d2b2b
Manohar Udutha <manohar.udutha@khumbu.co.in>
2024-06-25 16:30:41 +0530

    Authorization and logging added through middleware pipelining

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("net.sizovs:pipelinr:0.8")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
+    implementation("org.apache.logging.log4j:log4j-api:2.17.2")
 
 }
 
--- src/main/kotlin/Definitions.kt
+++ src/main/kotlin/Definitions.kt
@@ -1,10 +1,3 @@
 import an.awesome.pipelinr.Command
 
-data class MyCommand(val data: String) : Command<MyCommandResult>
-data class MyCommandResult(val result: String)
-
-data class MyQuery(val parameter: String) : Command<MyQueryResult>
-data class MyQueryResult(val data: String)
-
-data class MyNotification(val message: String) : Command<Void>
-
+data class MyCommand(val data: String) : Command<String>
